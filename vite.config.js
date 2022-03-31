a523db41af0bc2192ad069f65c1240f2b4c3d16d
tnqcs2022 <quangb1913331@student.ctu.edu.vn>
2022-03-31 15:04:33 +0700

    Khoi tao du an

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +13,11 @@ export default defineConfig({
   },
   server: {
       port: 8081,
+      proxy: {
+        "/api": {
+            target: "http://localhost:8080/",
+            changeOrigin: true,
+          },
+    }
   }
 })
